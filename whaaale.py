POLSL-SE/whaaale

commit 0e1346ae3341e8b525bef21fef04ee35751ec2a5

Prepare a starting point for MainWindow

whaaale.py
@@ -5,32 +5,42 @@ import numpy as np
 from PyQt6.QtCharts import QChart, QChartView, QLineSeries
 from PyQt6.QtGui import QPainter
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from osgeo import gdal
-
-gdal.UseExceptions()
-
-a = QApplication(argv)
-series = QLineSeries()
-
-series.append(0, 6)
-series.append(2, 4)
-l = np.array([[3, 8], [7, 4], [10, 5], [11, 1], [13, 3], [17, 6], [18, 3], [20, 2]])
-for p in l:
-    series.append(p[0], p[1])
 
 
-chart = QChart()
-chart.legend().hide()
-chart.addSeries(series)
-chart.createDefaultAxes()
-chart.setTitle("Simple line chart example")
+class MainWindow(QMainWindow):
+    def start(self):
+        self.setup_ui()
+        self.resize(400, 300)
+        self.show()
 
-chartView = QChartView(chart)
-chartView.setRenderHint(QPainter.RenderHint.Antialiasing)
+    def setup_ui(self):
+        series = QLineSeries()
 
-window = QMainWindow()
-window.setCentralWidget(chartView)
-window.resize(400, 300)
-window.show()
+        series.append(0, 6)
+        series.append(2, 4)
+        l = np.array(
+            [[3, 8], [7, 4], [10, 5], [11, 1], [13, 3], [17, 6], [18, 3], [20, 2]]
+        )
+        for p in l:
+            series.append(p[0], p[1])
 
-a.exec()
+        chart = QChart()
+        chart.legend().hide()
+        chart.addSeries(series)
+        chart.createDefaultAxes()
+        chart.setTitle("Simple line chart example")
+
+        chart_view = QChartView(chart)
+        chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        self.chart_view = chart_view
+        self.setCentralWidget(self.chart_view)
+
+
+def main():
+    a = QApplication(argv)
+    a.exec()
+
+
+if __name__ == "__main__":
+    main()

commit 984ebbc2b39e79654557c0634702581eca55c705

Show the application when started
Fixes a regression introduced in 0e1346ae3341e8b525bef21fef04ee35751ec2a5

whaaale.py
@@ -11,6 +11,7 @@ class MainWindow(QMainWindow):
     def start(self):
         self.setup_ui()
         self.resize(400, 300)
+        self.setWindowTitle("Whaaale")
         self.show()
 
     def setup_ui(self):
@@ -39,6 +40,8 @@ class MainWindow(QMainWindow):
 
 def main():
     a = QApplication(argv)
+    main_window = MainWindow()
+    main_window.start()
     a.exec()
 
 