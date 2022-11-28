POLSL-SE/whaaale

commit 0e1346ae3341e8b525bef21fef04ee35751ec2a5

Prepare a starting point for MainWindow

whaaale.py
@@ -5,32 +5,42 @@ import numpy as np
 from PyQt6.QtCharts import QChart, QChartView, QLineSeries
 from PyQt6.QtGui import QPainter
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from osgeo import gdal
-
-gdal.UseExceptions()
-
-a = QApplication(argv)
-series = QLineSeries()
-
-series.append(0, 6)
-series.append(2, 4)
-l = np.array([[3, 8], [7, 4], [10, 5], [11, 1], [13, 3], [17, 6], [18, 3], [20, 2]])
-for p in l:
-    series.append(p[0], p[1])
 
 
-chart = QChart()
-chart.legend().hide()
-chart.addSeries(series)
-chart.createDefaultAxes()
-chart.setTitle("Simple line chart example")
+class MainWindow(QMainWindow):
+    def start(self):
+        self.setup_ui()
+        self.resize(400, 300)
+        self.show()
 
-chartView = QChartView(chart)
-chartView.setRenderHint(QPainter.RenderHint.Antialiasing)
+    def setup_ui(self):
+        series = QLineSeries()
 
-window = QMainWindow()
-window.setCentralWidget(chartView)
-window.resize(400, 300)
-window.show()
+        series.append(0, 6)
+        series.append(2, 4)
+        l = np.array(
+            [[3, 8], [7, 4], [10, 5], [11, 1], [13, 3], [17, 6], [18, 3], [20, 2]]
+        )
+        for p in l:
+            series.append(p[0], p[1])
 
-a.exec()
+        chart = QChart()
+        chart.legend().hide()
+        chart.addSeries(series)
+        chart.createDefaultAxes()
+        chart.setTitle("Simple line chart example")
+
+        chart_view = QChartView(chart)
+        chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        self.chart_view = chart_view
+        self.setCentralWidget(self.chart_view)
+
+
+def main():
+    a = QApplication(argv)
+    a.exec()
+
+
+if __name__ == "__main__":
+    main()

commit e436859261cef6b7560b9b027668982ddfb1df5a

Prepare a starting point for the project (#1)
* Initial project configuration

* Add pyinstaller

* Add (linux amd64) build action

* Fix build action

* Try fixing missing *xcb*.so* files

* Try manually adding missing file

* Correct binary path

* Add another missing library

* Small build action cleanup

* Add windows build action

* Windows build: archive with directory

* Update dependencies, add scipy

* Test bundling GDAL as dependency

* Add h5py to dependencies

* Prepare a starting point for MainWindow

* Clarify h5py dependency status

* Show the application when started

Fixes a regression introduced in 0e1346ae3341e8b525bef21fef04ee35751ec2a5

* Don't run actions on every push

* Include information about builds in README

.editorconfig
@@ -1,0 +1,15 @@
+root = true
+
+[*]
+end_of_line = lf
+charset = utf-8
+insert_final_newline = true
+trim_trailing_whitespace = true
+
+[*.py]
+indent_style = space
+indent_size = 4
+
+[*.yml]
+indent_style = space
+indent_size = 2

.github/workflows/black.yml
@@ -1,0 +1,14 @@
+name: Lint
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+
+jobs:
+  lint:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - uses: psf/black@stable

.github/workflows/build.yml
@@ -1,0 +1,62 @@
+name: Build
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+
+jobs:
+  linux-amd64:
+    runs-on: ubuntu-22.04
+    steps:
+      # Checkout and setup
+      - uses: actions/checkout@v3
+      - uses: actions/setup-python@v4
+        with:
+          python-version: '3.11'
+          cache: 'pip'
+          cache-dependency-path: requirements.txt
+      - name: Install libraries (apt)
+        run: sudo apt install -y libegl1 libxcb*-dev libxkbcommon-x11-0 libgdal-dev libhdf5-dev
+      - name: Install pip dependencies
+        run: pip install -r requirements.txt
+
+      # Build
+      - name: Build
+        run: pyinstaller --noupx --add-binary "$(python -c 'import PyQt6 as q; print(q.__path__[0])')/Qt6/lib/libQt6XcbQpa.so.6":. whaaale.py
+
+      # Package and upload
+      - name: Package and compress
+        # Don't compress. GitHub packs artifacts into zip. Use tar to keep executable flag on binaries.
+        run: cd dist && tar -cvf whaaale-linux_amd64.tar whaaale/
+      - uses: actions/upload-artifact@v3
+        with:
+          name: whaaale-linux_amd64
+          path: dist/whaaale-linux_amd64.tar
+          if-no-files-found: error
+
+  windows-x64:
+    runs-on: windows-latest
+    steps:
+      # Checkout and setup
+      - uses: actions/checkout@v3
+      - uses: actions/setup-python@v4
+        with:
+          python-version: '3.11'
+          cache: 'pip'
+          cache-dependency-path: requirements.txt
+      - name: Install pip dependencies
+        run: pip install -r requirements.txt
+
+      # Build
+      - name: Build
+        run: pyinstaller --noconsole --noupx whaaale.py
+
+      # Upload
+      - uses: actions/upload-artifact@v3
+        with:
+          name: whaaale-windows_x64
+          # Upload dist, so that the archive will contain "whaaale" directory
+          path: dist/
+          if-no-files-found: error

whaaale.py
@@ -1,0 +1,49 @@
+# This is a demo to test packaging
+from sys import argv
+
+import numpy as np
+from PyQt6.QtCharts import QChart, QChartView, QLineSeries
+from PyQt6.QtGui import QPainter
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def start(self):
+        self.setup_ui()
+        self.resize(400, 300)
+        self.setWindowTitle("Whaaale")
+        self.show()
+
+    def setup_ui(self):
+        series = QLineSeries()
+
+        series.append(0, 6)
+        series.append(2, 4)
+        l = np.array(
+            [[3, 8], [7, 4], [10, 5], [11, 1], [13, 3], [17, 6], [18, 3], [20, 2]]
+        )
+        for p in l:
+            series.append(p[0], p[1])
+
+        chart = QChart()
+        chart.legend().hide()
+        chart.addSeries(series)
+        chart.createDefaultAxes()
+        chart.setTitle("Simple line chart example")
+
+        chart_view = QChartView(chart)
+        chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        self.chart_view = chart_view
+        self.setCentralWidget(self.chart_view)
+
+
+def main():
+    a = QApplication(argv)
+    main_window = MainWindow()
+    main_window.start()
+    a.exec()
+
+
+if __name__ == "__main__":
+    main()