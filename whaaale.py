29197bb9328d5334101fbbbc5e207f766194c6f8
przemek <przemyslaw.baca@nask.pl>
2022-11-30 22:48:20 +0100

    init

--- whaaale.py
+++ whaaale.py
@@ -21,34 +21,34 @@ class MainWindow(QMainWindow):
         self.size1 = 20
 
         # Setup funcs
-        self.setup_chart()
+        # self.setup_chart()
         self.resize(640, 360)
         self.setup_ui()
         self.setWindowTitle("Whaaale")
         self.show()
 
-    def setup_chart(self):
-        series = QLineSeries()
+    # def setup_chart(self):
+    #     series = QLineSeries()
 
-        series.append(0, 6)
-        series.append(2, 4)
-        l = np.array(
-            [[3, 8], [7, 4], [10, 5], [11, 1], [13, 3], [17, 6], [18, 3], [20, 2]]
-        )
-        for p in l:
-            series.append(p[0], p[1])
+    #     series.append(0, 6)
+    #     series.append(2, 4)
+    #     l = np.array(
+    #         [[3, 8], [7, 4], [10, 5], [11, 1], [13, 3], [17, 6], [18, 3], [20, 2]]
+    #     )
+    #     for p in l:
+    #         series.append(p[0], p[1])
 
-        chart = QChart()
-        chart.legend().hide()
-        chart.addSeries(series)
-        chart.createDefaultAxes()
-        chart.setTitle("Simple line chart example")
+    #     chart = QChart()
+    #     chart.legend().hide()
+    #     chart.addSeries(series)
+    #     chart.createDefaultAxes()
+    #     chart.setTitle("Simple line chart example")
 
-        chart_view = QChartView(chart)
-        chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
+    #     chart_view = QChartView(chart)
+    #     chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
 
-        self.chart_view = chart_view
-        self.setCentralWidget(self.chart_view)
+    #     self.chart_view = chart_view
+    #     self.setCentralWidget(self.chart_view)
 
     # Overridden function so we can adjust gui to the size of the window
     def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
@@ -60,41 +60,41 @@ class MainWindow(QMainWindow):
         return super().resizeEvent(a0)
 
     def setup_ui(self):
-        layout = QGridLayout()
-
-        self.single_band_button = QPushButton(self)
-        self.single_band_button.setText('Single')
-        self.single_band_button.setGeometry(200, 50, 20, 20)
-        layout.addWidget(self.single_band_button)
-
-        widget = QWidget()
-        widget.resize(100, 100)
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
-        
-        # # Menu bar
-        # self._createMenuBar()
-
-        # # Buttons
-        # # Create a QVBoxLayout instance
+        # layout = QGridLayout()
 
         # self.single_band_button = QPushButton(self)
         # self.single_band_button.setText('Single')
-        # # self.single_band_button.setSize
-        # # self.single_band_button.move(5, 30)
+        # self.single_band_button.setGeometry(200, 50, 20, 20)
+        # layout.addWidget(self.single_band_button)
+
+        # widget = QWidget()
+        # widget.resize(100, 100)
+        # widget.setLayout(layout)
+        # self.setCentralWidget(widget)
+        
+        # Menu bar
+        self._createMenuBar()
+
+        # Buttons
+        # Create a QVBoxLayout instance
+
+        self.single_band_button = QPushButton(self)
+        self.single_band_button.setText('Single')
+        # self.single_band_button.setSize
+        self.single_band_button.move(5, 30)
         # self.single_band_button.setGeometry(100, 100, self.size1, self.size1)
-        # self.single_band_button.clicked.connect(self.single_band)
+        self.single_band_button.clicked.connect(self.single_band)
 
-        # self.fake_col_button = QPushButton(self)
-        # self.fake_col_button.setText('Fake color')
-        # self.fake_col_button.move(110, 30)
-        # self.fake_col_button.clicked.connect(self.fake_col)
+        self.fake_col_button = QPushButton(self)
+        self.fake_col_button.setText('Fake color')
+        self.fake_col_button.move(110, 30)
+        self.fake_col_button.clicked.connect(self.fake_col)
 
-        # frame = QFrame(self)
-        # frame.setFrameShape(QFrame.Shape.StyledPanel)
-        # frame.setLineWidth(3)
-        # frame.resize(100, 100)
-        # frame.move(self.size1, 200)
+        frame = QFrame(self)
+        frame.setFrameShape(QFrame.Shape.StyledPanel)
+        frame.setLineWidth(3)
+        frame.resize(100, 100)
+        frame.move(self.size1, 200)
         
     def single_band(self):
         # Function for handling clicking on single_band_button
