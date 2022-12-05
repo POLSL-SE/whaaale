46bf8714e1a46f9f82798f90d56f1e4cdf4cb258
krzysdz <krzysdz@users.noreply.github.com>
2022-12-06 00:44:21 +0100

    Switch to matplotlib

--- ui/spectral_viewer.py
+++ ui/spectral_viewer.py
@@ -1,14 +1,20 @@
 from dataclasses import dataclass
+from math import ceil
 from typing import Literal, Optional
 
+import matplotlib.colors
+import matplotlib.style
 import numpy as np
+from matplotlib.axes import Axes
+from matplotlib.backends.backend_qt import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 from numpy.typing import NDArray
-from PyQt6.QtCharts import QCategoryAxis, QChart, QChartView, QLineSeries
-from PyQt6.QtCore import Qt, QMargins
-from PyQt6.QtGui import QFont, QPainter, QMouseEvent
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import QGridLayout, QLabel, QSizePolicy, QWidget
 
-from lib import ScalarType
+from lib import LabelType, ScalarType
 
 
 @dataclass
@@ -27,7 +33,7 @@ class AreaValues:
 
 class SpectralViewer(QWidget):
     data: Optional[PixelValues | AreaValues] = None
-    chart_view: Optional[QChartView] = None
+    canvas: Optional[FigureCanvas] = None
 
     def __init__(
         self,
@@ -35,6 +41,10 @@ class SpectralViewer(QWidget):
         flags: Qt.WindowType = Qt.WindowType.Widget,
     ) -> None:
         super().__init__(parent, flags)
+
+        # Force Qt backend and set style
+        matplotlib.use("qtagg")
+        matplotlib.style.use("style/dark_plot.mplstyle")
 
         status_label = QLabel(
             "Open an image and select a pixel or an area to show the spectral curve.",
@@ -57,12 +67,13 @@ class SpectralViewer(QWidget):
         self.setLayout(grid_layout)
         self.grid_layout = grid_layout
 
-    def update_labels(self, labels: list[str]):
+    def update_labels(self, labels: list[str], labels_type: LabelType):
         self.labels = labels
+        self.labels_type = labels_type
 
     def from_pixel(self, pixel: NDArray):
         self.data = PixelValues(pixel)
-        self.render_pixel()
+        self.render()
 
     def from_area(self, area: NDArray[ScalarType]):
         h, w, b = area.shape
@@ -75,14 +86,18 @@ class SpectralViewer(QWidget):
             avg=avg, min=v_min, max=v_max, quartile_low=q_low, quartile_high=q_high
         )
         self.data = values
-        self.render_area()
+        self.render()
 
     def clear(self):
         self.data = None
 
-        if self.chart_view is not None:
-            self.grid_layout.removeWidget(self.chart_view)
-            self.chart_view = None
+        if self.canvas is not None:
+            self.grid_layout.removeWidget(self.canvas)
+            self.grid_layout.removeWidget(self.toolbar)
+            self.canvas.deleteLater()
+            self.toolbar.deleteLater()
+            self.canvas = None
+            self.toolbar = None
 
         self.status_label.setText(
             "Select a pixel or an area to show the spectral curve."
@@ -90,126 +105,158 @@ class SpectralViewer(QWidget):
         self.status_label.setVisible(True)
         self.grid_layout.addWidget(self.status_label)
 
-    def render_pixel(self):
-        if not isinstance(self.data, PixelValues):
-            raise RuntimeError(
-                "Pixel spectral curve render requested, but data is not a pixel value (either area or None)."
-            )
+    def render(self):
+        if self.data is None:
+            raise RuntimeError("Spectral curve render requested, but data is None")
+        if self.labels is None:
+            raise RuntimeError("Labels have not been provided")
 
-        series = QLineSeries()
-        series.setName("Value")
+        bands = len(self.labels)
+        fig = Figure(tight_layout=True)
+        canvas = FigureCanvas(fig)
+        ax: Axes = fig.subplots()
 
-        for i, val in enumerate(self.data.values):
-            series.append(float(i), float(val))
+        match self.labels_type:
+            case LabelType.CUSTOM_STR:
+                ax.set_xticks(
+                    np.arange(bands),
+                    labels=self.labels,
+                    rotation=90,
+                    fontsize="x-small",
+                )
+                size = fig.get_size_inches() * fig.dpi
+                w = size[0]
+                step = ceil(bands / w * 12)
+                for i, label in enumerate(ax.xaxis.get_ticklabels()):
+                    if (i % step) != 0:
+                        label.set_visible(False)
 
-        chart = QChart()
-        chart.addSeries(series)
-        chart.createDefaultAxes()
-        chart.setTitle("Spectral curve")
-        chart.setTheme(QChart.ChartTheme.ChartThemeDark)
+                x_values = np.arange(bands)
+                ax.set_xlabel("Band")
 
-        margins = QMargins(2, 2, 2, -7)
-        chart.setMargins(margins)
+            case LabelType.WAVELENGTH:
+                x_values = np.array(self.labels, dtype=np.float64)
+                self.show_spectrum_bg(ax, self.data, x_values)
+                ax.set_xlabel("Wavelength [nm]")
 
-        legend = chart.legend()
-        legend.setVisible(False)
-        legend.setAlignment(Qt.AlignmentFlag.AlignBottom)
+            case LabelType.AUTO:
+                x_values = np.arange(bands)
+                ax.set_xlabel("Band")
 
-        chart_view = QChartView(chart)
-        chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
-        chart_view.setRubberBand(QChartView.RubberBand.RectangleRubberBand)
-        chart_view.mouseDoubleClickEvent = lambda event: self.chart_double_click(event)
+        match self.data:
+            case AreaValues(avg, min, max, quartile_low, quartile_high):
+                ax.plot(x_values, avg, label="avg")
+                ax.plot(x_values, min, label="min")
+                ax.plot(x_values, max, label="max")
+                ax.plot(x_values, quartile_low, label="25%")
+                ax.plot(x_values, quartile_high, label="75%")
 
-        try:
-            self.grid_layout.removeWidget(self.status_label)
-        except Exception as err:
-            print(err)
+            case PixelValues(values):
+                ax.plot(x_values, values, label="Value")
 
-        if self.chart_view is not None:
-            self.grid_layout.removeWidget(self.chart_view)
+            case None:
+                raise RuntimeError("No value")
 
-        self.grid_layout.addWidget(chart_view)
-        self.chart_view = chart_view
+        ax.margins(0, 0)
+        ax.legend()
 
-    def render_area(self):
-        if not isinstance(self.data, AreaValues):
-            raise RuntimeError(
-                "Area spectral curve render requested, but data is not an `AreaValues` object, but a pixel or None."
-            )
+        self.status_label.setVisible(False)
+        self.grid_layout.removeWidget(self.status_label)
+        toolbar = NavigationToolbar(canvas, self)
+        if self.canvas:
+            self.grid_layout.replaceWidget(self.canvas, canvas)
+            self.grid_layout.replaceWidget(self.toolbar, toolbar)
+        else:
+            self.grid_layout.addWidget(canvas)
+            self.grid_layout.addWidget(toolbar)
+        self.canvas = canvas
+        self.toolbar = toolbar
 
-        series_min = QLineSeries()
-        series_min.setName("min")
-        series_qlow = QLineSeries()
-        series_qlow.setName("25%")
-        series_avg = QLineSeries()
-        series_avg.setName("avg")
-        series_qhigh = QLineSeries()
-        series_qhigh.setName("75%")
-        series_max = QLineSeries()
-        series_max.setName("max")
-
-        for i in range(len(self.labels)):
-            x = float(i)
-            series_min.append(x, self.data.min[i])
-            series_qlow.append(x, self.data.quartile_low[i])
-            series_avg.append(x, self.data.avg[i])
-            series_qhigh.append(x, self.data.quartile_high[i])
-            series_max.append(x, self.data.max[i])
-
-        chart = QChart()
-        chart.addSeries(series_min)
-        chart.addSeries(series_qlow)
-        chart.addSeries(series_avg)
-        chart.addSeries(series_qhigh)
-        chart.addSeries(series_max)
-        chart.setTitle("Spectral curve")
-        chart.setTheme(QChart.ChartTheme.ChartThemeDark)
-
-        margins = QMargins(2, 2, 2, -7)
-        chart.setMargins(margins)
-
-        chart.createDefaultAxes()
-        default_x_axis = chart.axes(Qt.Orientation.Horizontal)[0]
-        chart.removeAxis(default_x_axis)
-        axis = self.label_x_axis()
-        chart.addAxis(axis, Qt.AlignmentFlag.AlignBottom)
-
-        legend = chart.legend()
-        legend.setVisible(True)
-        legend.setAlignment(Qt.AlignmentFlag.AlignBottom)
-
-        chart_view = QChartView(chart)
-        chart_view.setRenderHint(QPainter.RenderHint.Antialiasing)
-        chart_view.setRubberBand(QChartView.RubberBand.RectangleRubberBand)
-        chart_view.mouseDoubleClickEvent = lambda event: self.chart_double_click(event)
-
-        try:
-            self.status_label.hide()
-            self.grid_layout.removeWidget(self.status_label)
-        except Exception as err:
-            print(err)
-
-        if self.chart_view is not None:
-            self.grid_layout.removeWidget(self.chart_view)
-
-        self.grid_layout.addWidget(chart_view)
-        self.chart_view = chart_view
-
-    def label_x_axis(self) -> QCategoryAxis:
-        n_labels = len(self.labels)
-        axis = QCategoryAxis()
-        axis.setMin(0)
-        axis.setMax(n_labels - 1)
-        axis.setStartValue(0)
-        axis.setLabelsPosition(
-            QCategoryAxis.AxisLabelsPosition.AxisLabelsPositionOnValue
+    def show_spectrum_bg(
+        self, ax: Axes, values: AreaValues | PixelValues, x_values: NDArray[np.float64]
+    ):
+        # Visible spectrum limits for the image
+        clim = (350, 780)
+        # Prepare normalizer scaling [350, 780] to [0, 1]
+        norm = matplotlib.colors.Normalize(*clim)
+        # List of wavelengths from the visible light spectrum every 2 nm
+        wl = np.arange(clim[0], clim[1] + 1, 2)
+        # Prepare a `(wavelength, RGBA colour)` list
+        colorlist = list(zip(norm(wl), [wavelength_to_rgb(w) for w in wl]))
+        # Prepare a colormap for `imshow`
+        spectralmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+            "spectrum", colorlist
         )
-        for i, label in enumerate(self.labels):
-            axis.append(label, i)
 
-        return axis
+        if isinstance(values, AreaValues):
+            v_min = np.min(values.min)
+            v_max = np.max(values.max)
+        else:
+            v_min = np.min(values.values)
+            v_max = np.max(values.values)
 
-    def chart_double_click(self, event: QMouseEvent):
-        if self.chart_view is not None:
-            self.chart_view.chart().zoomReset()
-            event.accept()
+        x_min = np.min(x_values)
+        x_max = np.max(x_values)
+        x_stripes = np.linspace(x_min, x_max, 1000)
+
+        y = np.linspace(v_min, v_max, 100)
+        X, Y = np.meshgrid(x_stripes, y)
+
+        extent = (x_min, x_max, v_min, v_max)
+
+        ax.imshow(
+            X, clim=clim, extent=extent, cmap=spectralmap, aspect="auto", alpha=0.5
+        )
+
+
+# From https://stackoverflow.com/a/44960748
+def wavelength_to_rgb(wavelength: float, gamma: float = 0.8):
+    """taken from http://www.noah.org/wiki/Wavelength_to_RGB_in_Python
+    This converts a given wavelength of light to an
+    approximate RGB color value. The wavelength must be given
+    in nanometers in the range from 380 nm through 750 nm
+    (789 THz through 400 THz).
+
+    Based on code by Dan Bruton
+    http://www.physics.sfasu.edu/astro/color/spectra.html
+    Additionally alpha value set to 0.5 outside range
+    """
+    if wavelength >= 380 and wavelength <= 750:
+        A = 1.0
+    else:
+        A = 0.2
+    if wavelength < 380:
+        wavelength = 380.0
+    if wavelength > 750:
+        wavelength = 750.0
+    if wavelength >= 380 and wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        R: float = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+        G: float = 0.0
+        B: float = (1.0 * attenuation) ** gamma
+    elif wavelength >= 440 and wavelength <= 490:
+        R = 0.0
+        G = ((wavelength - 440) / (490 - 440)) ** gamma
+        B = 1.0
+    elif wavelength >= 490 and wavelength <= 510:
+        R = 0.0
+        G = 1.0
+        B = (-(wavelength - 510) / (510 - 490)) ** gamma
+    elif wavelength >= 510 and wavelength <= 580:
+        R = ((wavelength - 510) / (580 - 510)) ** gamma
+        G = 1.0
+        B = 0.0
+    elif wavelength >= 580 and wavelength <= 645:
+        R = 1.0
+        G = (-(wavelength - 645) / (645 - 580)) ** gamma
+        B = 0.0
+    elif wavelength >= 645 and wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        R = (1.0 * attenuation) ** gamma
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+    return (R, G, B, A)
