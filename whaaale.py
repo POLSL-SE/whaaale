fdb39666b398c74dc5dde0c7a155ac591e50f326
przemek <przemyslaw.baca@nask.pl>
2022-12-09 09:03:23 +0100

    v2

--- whaaale.py
+++ whaaale.py
@@ -11,7 +11,7 @@ from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def start(self):
-        self.resize(640, 360)
+        self.resize(1280, 720)
         self.setup_ui()
         self.setWindowTitle("Whaaale")
         self.show()
@@ -37,21 +37,20 @@ class MainWindow(QMainWindow):
 
         self.single_band_button = QPushButton(self)
         self.single_band_button.setText('Single')
-        self.single_band_button.clicked.connect(self.single_band)
+        self.single_band_button.clicked.connect(self.single_band_click)
 
         self.fake_col_button = QPushButton(self)
         self.fake_col_button.setText('Fake color')
-        self.fake_col_button.clicked.connect(self.fake_col)
+        self.fake_col_button.clicked.connect(self.fake_col_click)
 
         # ****** Magic Wand ******
 
         self.label_wand = QLabel("Magic wand", self)
         self.label_wand.setFixedSize(90, 20)
 
-        select_magic_wand = QComboBox()
-        select_magic_wand.addItem("Option 1")
-        select_magic_wand.addItem("Option 2")
-        select_magic_wand.addItem("Option 3")
+        self.button_magic = QPushButton(self)
+        self.button_magic.setText('Magic wand')
+        self.button_magic.clicked.connect(self.magic_wand_click)
 
         slider_magic_wand = QSlider(Qt.Orientation.Horizontal, self)
         slider_magic_wand.setGeometry(50,50, 200, 50)
@@ -67,23 +66,30 @@ class MainWindow(QMainWindow):
 
         self.select_point = QPushButton(self)
         self.select_point.setText('Select point')
-        self.select_point.clicked.connect(self.single_band)
+        self.select_point.clicked.connect(self.select_point_click)
 
         self.select_area = QPushButton(self)
         self.select_area.setText('Select area')
-        self.select_area.clicked.connect(self.fake_col)
+        self.select_area.clicked.connect(self.select_area_click)
+
+        self.label_export = QLabel("Export", self)
+        self.label_export.setFixedSize(60, 20)
 
         self.export_png = QPushButton(self)
         self.export_png.setText('PNG')
-        self.export_png.clicked.connect(self.single_band)
+        self.export_png.clicked.connect(self.export_click)
         self.export_png.setFixedSize(35, 35)
 
         self.export_csv = QPushButton(self)
         self.export_csv.setText('CSV')
-        self.export_csv.clicked.connect(self.fake_col)
+        self.export_csv.clicked.connect(self.export_click)
         self.export_csv.setFixedSize(35, 35)
 
         # ****** Menu ******
+
+        select_mode = QComboBox()
+        select_mode.addItem("Mono")
+        select_mode.addItem("RGB")
 
         self.label_band1 = QLabel("Band #1", self)
         self.label_band1.setFixedSize(40, 20)
@@ -117,21 +123,23 @@ class MainWindow(QMainWindow):
         toolbar1.addWidget(self.fake_col_button)
 
         toolbar2.addWidget(self.label_wand)
-        toolbar2.addWidget(select_magic_wand)
+        toolbar2.addWidget(self.button_magic)
         toolbar2.addWidget(slider_magic_wand)
 
         toolbar3.addWidget(self.label_spectral, 0, 0)
         toolbar3.addWidget(self.select_point, 1, 0)
         toolbar3.addWidget(self.select_area, 2, 0)
-        toolbar3.addWidget(self.export_png, 3, 0)
-        toolbar3.addWidget(self.export_csv, 3, 1)
+        toolbar3.addWidget(self.label_export,3,0)
+        toolbar3.addWidget(self.export_png, 4, 0)
+        toolbar3.addWidget(self.export_csv, 4, 1)
 
-        toolbar4.addWidget(self.label_band1, 0, 0)
-        toolbar4.addWidget(frame1, 0, 1)
-        toolbar4.addWidget(self.label_band2, 1, 0)
-        toolbar4.addWidget(frame2, 1, 1)
-        toolbar4.addWidget(self.label_band3, 2, 0)
-        toolbar4.addWidget(frame3, 2, 1)
+        toolbar4.addWidget(select_mode, 0, 0)
+        toolbar4.addWidget(self.label_band1, 1, 1)
+        toolbar4.addWidget(frame1, 1, 2)
+        toolbar4.addWidget(self.label_band2, 2, 1)
+        toolbar4.addWidget(frame2, 2, 2)
+        toolbar4.addWidget(self.label_band3, 3, 1)
+        toolbar4.addWidget(frame3, 3, 2)
 
         spectrum_graph.addWidget(self.setup_chart())
         viewer.addWidget(self.setup_chart())
@@ -168,14 +176,6 @@ class MainWindow(QMainWindow):
         return chart_view
         # self.chart_view = chart_view
         # self.setCentralWidget(self.chart_view)      
-        
-    def single_band(self):
-        # Function for handling clicking on single_band_button
-        print('clicked single band')
-
-    def fake_col(self):
-        # Function for handling clicking on fake_col_button
-        print('clicked fake color')
 
     def _createMenuBar(self):
         menuBar = QMenuBar(self)
@@ -186,27 +186,49 @@ class MainWindow(QMainWindow):
 
         # File menu
         action_new = QAction("New", self)
-        action_new.triggered.connect(self.fake_col)
+        action_new.triggered.connect(self.fake_col_click)
         action_open = QAction("Open", self)
-        action_open.triggered.connect(self.fake_col)
+        action_open.triggered.connect(self.fake_col_click)
         action_exit = QAction("Exit", self)
-        action_exit.triggered.connect(self.fake_col)
+        action_exit.triggered.connect(self.fake_col_click)
         fileMenu.addAction(action_new)
         fileMenu.addAction(action_open)
         fileMenu.addAction(action_exit)
 
         # Edit menu
         action_view1 = QAction("Option 1", self)
-        action_view1.triggered.connect(self.fake_col)
+        action_view1.triggered.connect(self.fake_col_click)
         action_view2 = QAction("Option 2", self)
-        action_view2.triggered.connect(self.fake_col)
+        action_view2.triggered.connect(self.fake_col_click)
         editMenu.addAction(action_view1)
         editMenu.addAction(action_view2)
 
         # Help menu
         action_help = QAction("Help", self)
-        action_help.triggered.connect(self.fake_col)
+        action_help.triggered.connect(self.fake_col_click)
         helpMenu.addAction(action_help)
+
+    """Methods responsible for handling interaction with buttons etc."""
+
+    def single_band_click(self):
+        print('clicked single band')
+
+    def fake_col_click(self):
+        print('clicked fake color')
+
+    def magic_wand_click(self):
+        print('clicked magic wand')
+
+    def select_point_click(self):
+        print('clicked select point')
+
+    def select_area_click(self):
+        print('clicked select area')
+
+    def export_click(self):
+        print('clicked export')
+
+
 
 
 def main():
